126c433344310c38a6900f65e5af07204dd4c0b8
pavan kumar <pavan.yalamanchi@gmail.com>
2021-07-06 16:59:31 +0100

    updating with genres

--- src/components/latestRelease.jsx
+++ src/components/latestRelease.jsx
@@ -1,13 +1,17 @@
 import {Component} from 'react'
 import {Container, Row, Col, Carousel} from 'react-bootstrap'
 import items from '../data/fantasy.json'
+import history from '../data/history.json'
+import romance from '../data/romance.json'
+import horror from '../data/horror.json'
+import scifi from '../data/scifi.json'
 
 class LatestRelease extends Component{
     render(){
         return(
             <Container>
                 <Row className="justify-content-center mt-3">
-                    <Col xs={12} md={6}>
+                    <Col xs={12} md={2} lg={2}>
                         <Carousel>
                             {
                                 items.map(item => (
@@ -17,7 +21,75 @@ class LatestRelease extends Component{
                                             src={item.img}
                                             alt="First slide"
                                         />
-                                            <h3>{item.title}</h3>
+                                            
+                                            <p>{item.category}</p>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
+                    </Col>
+                    <Col xs={12} md={2} lg={2}>
+                        <Carousel>
+                            {
+                                history.map(item => (
+                                    <Carousel.Item key={item.id}>
+                                        <img
+                                            className="d-block w-100"
+                                            src={item.img}
+                                            alt="First slide"
+                                        />
+                                            
+                                            <p>{item.category}</p>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
+                    </Col>
+                    <Col xs={12} md={2} lg={2}>
+                        <Carousel>
+                            {
+                                horror.map(item => (
+                                    <Carousel.Item key={item.id}>
+                                        <img
+                                            className="d-block w-100"
+                                            src={item.img}
+                                            alt="First slide"
+                                        />
+                                           
+                                            <p>{item.category}</p>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
+                    </Col>
+                    <Col xs={12} md={2} lg={2}>
+                        <Carousel>
+                            {
+                                romance.map(item => (
+                                    <Carousel.Item key={item.id}>
+                                        <img
+                                            className="d-block w-100"
+                                            src={item.img}
+                                            alt="First slide"
+                                        />
+                                           
+                                            <p>{item.category}</p>
+                                    </Carousel.Item>
+                                ))
+                            }
+                        </Carousel>
+                    </Col>
+                    <Col xs={12} md={2} lg={2}>
+                        <Carousel>
+                            {
+                                scifi.map(item => (
+                                    <Carousel.Item key={item.id}>
+                                        <img
+                                            className="d-block w-100"
+                                            src={item.img}
+                                            alt="First slide"
+                                        />
+                                            
                                             <p>{item.category}</p>
                                     </Carousel.Item>
                                 ))
